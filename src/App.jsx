ebe87435f7956d4f5197251405dd8de42ef51ef9
Reretak2 <rere-botak@proton.me>
2026-04-15 17:38:53 +0700

    blublublub

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,70 @@
 import { useState } from 'react'
 import './App.css'
 import checkWindowDimension from './checkWindowDimension';
+import useHorizontalScroll  from './horizontalScroll';
 
 function App() {
+
+  const scrollRef = useHorizontalScroll();
+
   return (
-    <>
-      <Card id={"1"} boxtext={"2024-2026 / 514-516PU"} text={{maintext: "Whether intentionally or not, the organization had revealed the existence of another world to the public. Negotiation, threat, and backroom deals would fill many chambers during these years. While Earth's world leaders were busy, the leaders on the other side were in no way relaxed. Yet, without internet, information travels slowly. Only a small part of the world knew what a human was, but soon, they would know them very well.",titletext:"First Contact"}} boximg={"https://i.imgur.com/JevQysk.png"}/>
-      <Card id={"2"} boxtext={"2027-2030 / 517-520PU"} text={{maintext: "Real life wasn't full of sunshine and rainbows. Although the cultural exchange and aid program had gone off without a hitch, a new conflict brewed on the horizon. Meanwhile on earth, as speculation on magic reached a new feverish pitch, schemes began on bypassing the organization rules. The newly established United Nations Otherworldly Commission must handle one of the biggest events humankind has ever faced, and they are already tired.",titletext:"Early Conflict"}} boximg={"https://i.imgur.com/qNku1FH.jpeg"}/>
-      <Card id={"3"} boxtext={"2031-2040 / 521-530 PU"} text={{maintext: "The dust settled, with humanity appearing on top. Treaties were signed. And the overcumbered UNOC and the organization must handle Earth nations' creeping influence on the other world, lest it become another ground for proxy war and colonization. The organization, however, was too busy making a new weapon, one that the world had never seen and wished they never would. Meanwhile, on Earth, the situation began heating up. The question is no longer 'Will it collapse?' But 'When will it collapse?' And the answer seems to be very, very soon.",titletext:"Messy Days"}} boximg={"https://i.imgur.com/g0pj1sC.jpeg"}/>
-      <Card id={"4"} boxtext={"2041-2043 / 531-533PU"} text={{maintext: "With the UNOC influence left in the bin and the organization being fractured as ever. Earth nations began breaching in. Human ideas spread like a wildfire, and the otherworld seems to be infected, slowly but surely, with Earth descending to madness. On Earth, the clock had finally reached the number 12. ",titletext:"Beginning of an End"}} boximg={"https://i.imgur.com/DrHVWLG.jpeg"}/>
-      <Card id={"5"} boxtext={"2044-2060 / 534-550PU"} text={{maintext: "The portals were all closed. Leaving millions of humans stranded in another world. Their new predicament was made worse by raids, infighting, and general chaos. As their modern settlements fell into ruin, the humans set out across the world, accompanied by those who they had helped before, as allies. Though not fully without enemies. Despite the closures, ideas don't die. And a new conflict that the otherworld had never seen before seems inevitable. Though it might seem to be fresh problems for them, to humanity, these were old problems, from the fiery spreads of nationalism to the absolute mess that is industrialization. Would the otherworld follow in Earth history's footsteps, or would they carve their own path?",titletext:"Fresh Problems"}} boximg={"https://i.imgur.com/tS2Hiqz.jpeg"}/>
-    </>
+    <div ref={scrollRef} id="fakeroot">
+      <Card id={"1"} boxtext={"2024-2026 / 514-516PU"} text={{
+        maintext: "Whether intentionally or not, the organization had revealed the existence of another world to the public. Negotiation, threat, and backroom deals would fill many chambers during these years. While Earth's world leaders were busy, the leaders on the other side were in no way relaxed. Yet, without internet, information travels slowly. Only a small part of the world knew what a human was, but soon, they would know them very well.",
+        titletext:"First Contact",
+        stories: [
+          {id:0,name:"Humans dark secret",link:"https://www.reddit.com/r/HFY/comments/1kih1xq/humans_dark_secret"},
+          {id:1,name:"Overreaction",link:"https://www.reddit.com/r/HFY/comments/1o5lw9y/overreaction"},
+          {id:2,name:"Charisma",link:"https://www.reddit.com/r/HFY/comments/1oyjf1t/charisma"}
+        ]
+        }} 
+        boximg={"https://i.imgur.com/JevQysk.png"}/>
+      <Card id={"2"} boxtext={"2027-2030 / 517-520PU"} text={{
+        maintext: "Real life wasn't full of sunshine and rainbows. Although the cultural exchange and aid program had gone off without a hitch, a new conflict brewed on the horizon. Meanwhile on earth, as speculation on magic reached a new feverish pitch, schemes began on bypassing the organization rules. The newly established United Nations Otherworldly Commission must handle one of the biggest events humankind has ever faced, and they are already tired.",
+        titletext:"Early Conflict",
+        stories: [
+          {id:0,name:"Curiosity killed the catfolk",link:"https://www.reddit.com/r/HFY/comments/1nud823/curiosity_killed_the_catfolk"},
+          {id:1,name:"Easy prey",link:"https://www.reddit.com/r/HFY/comments/1o4lpex/easy_prey"},
+          {id:2,name:"An urgent letter",link:"https://www.reddit.com/r/HFY/comments/1omg6ms/an_urgent_letter"},
+          {id:3,name:"USE parliament_bills_db;",link:"https://www.reddit.com/r/HFY/comments/1oo6ecp/use_parliament_bills_db"},
+          {id:4,name:"Easy prey",link:"https://www.reddit.com/r/HFY/wiki/series/yes_i_am_a_human"},
+          {id:5,name:"An urgent letter",link:"https://www.reddit.com/r/HFY/comments/1pom3c2/mere_influence/"}
+        ]
+        }} 
+        boximg={"https://i.imgur.com/qNku1FH.jpeg"}/>
+      <Card id={"3"} boxtext={"2031-2040 / 521-530 PU"} text={{
+        maintext: "The dust settled, with humanity appearing on top. Treaties were signed. And the overcumbered UNOC and the organization must handle Earth nations' creeping influence on the other world, lest it become another ground for proxy war and colonization. The organization, however, was too busy making a new weapon, one that the world had never seen and wished they never would. Meanwhile, on Earth, the situation began heating up. The question is no longer 'Will it collapse?' But 'When will it collapse?' And the answer seems to be very, very soon.",
+        titletext:"Messy Days",
+        stories: [
+
+        ]
+        }} 
+        boximg={"https://i.imgur.com/g0pj1sC.jpeg"}/>
+      <Card id={"4"} boxtext={"2041-2043 / 531-533PU"} text={{
+        maintext: "With the UNOC influence left in the bin and the organization being fractured as ever. Earth nations began breaching in. Human ideas spread like a wildfire, and the otherworld seems to be infected, slowly but surely, with Earth descending to madness. On Earth, the clock had finally reached the number 12. ",
+        titletext:"Beginning of an End",
+        stories: [
+
+        ]
+        }}  
+        boximg={"https://i.imgur.com/DrHVWLG.jpeg"}/>
+      <Card id={"5"} boxtext={"2044-2060 / 534-550PU"} text={{
+        maintext: "The portals were all closed. Leaving millions of humans stranded in another world. Their new predicament was made worse by raids, infighting, and general chaos. As their modern settlements fell into ruin, the humans set out across the world, accompanied by those who they had helped before, as allies. Though not fully without enemies. Despite the closures, ideas don't die. And a new conflict that the otherworld had never seen before seems inevitable. Though it might seem to be fresh problems for them, to humanity, these were old problems, from the fiery spreads of nationalism to the absolute mess that is industrialization. Would the otherworld follow in Earth history's footsteps, or would they carve their own path?",
+        titletext:"Fresh Problems",
+        stories: [
+          {id:0,name:"Call to arms",link:"https://www.reddit.com/r/HFY/comments/1orocdw/call_to_arms"},
+          {id:1,name:"A step to far",link:"https://www.reddit.com/r/HFY/comments/1p17li4/a_step_to_far"},
+          {id:2,name:"The Downfall of slavery",link:"https://www.reddit.com/r/HFY/comments/1p2xrvb/the_downfall_of_slavery"},
+          {id:3,name:"An Undergraduate Draft Thesis",link:"https://www.reddit.com/r/HFY/comments/1p68vgn/an_undergraduate_draft_thesis"},
+          {id:4,name:"A silly thought",link:"https://www.reddit.com/r/HFY/comments/1pb200w/a_silly_thought"},
+          {id:5,name:"Just Humans being humans",link:"https://www.reddit.com/r/HFY/comments/1pd11vx/just_humans_being_humans"}
+        ]
+        }} 
+        boximg={"https://i.imgur.com/tS2Hiqz.jpeg"}/>
+    </div>
   )
 }
-function CardText({text}){
+function CardText({text,setIsHovered}){
   const { width, height } = checkWindowDimension();
   let textstuff;
   let subtextstuff;
@@ -26,7 +77,15 @@ function CardText({text}){
     subtextstuff = "1.25rem"
   }
   return (
-    <>
+    <div
+      onMouseEnter={()=>{setIsHovered(true)}}
+      onMouseLeave={()=>{setIsHovered(false)}}
+      onTouchStart={()=>{setIsHovered(true)}}
+      onTouchEnd={()=>{setIsHovered(false)}}
+      style={{
+        overflow: "auto"
+      }}
+    >
       <p
         style={{
           fontSize: textstuff,
@@ -44,7 +103,10 @@ function CardText({text}){
       >
         {text.maintext}
       </p>
-    </>
+        {text.stories.map((s) => {
+          return <p key={s.id}><a href={s.link}>{s.name}</a></p>
+        })}
+    </div>
   )
 }
 function Card({ id,text,boxtext,boximg }){
@@ -52,13 +114,14 @@ function Card({ id,text,boxtext,boximg }){
   let firstBox;
   let secondBox;
   let dir;
+  const [isHovered, setIsHovered] = useState(false);
   if(id % 2 == 0){
-    firstBox = <Box id={id}><Boximg boximg={boximg}/></Box>
-    secondBox = <Box id={id}><CardText text={text}></CardText></Box>
+    firstBox = <Box  isHovered={isHovered}><Boximg boximg={boximg}/></Box>
+    secondBox = <Box isHovered={isHovered}><CardText text={text} setIsHovered={setIsHovered}></CardText></Box>
   }
   else{
-    firstBox = <Box id={id}><CardText text={text}></CardText></Box>
-    secondBox = <Box id={id}><Boximg boximg={boximg}/></Box>
+    firstBox = <Box  isHovered={isHovered}><CardText text={text} setIsHovered={setIsHovered}></CardText></Box>
+    secondBox = <Box isHovered={isHovered}><Boximg boximg={boximg}/></Box>
   }
   if(width < 600){
     dir = "row"
@@ -66,6 +129,8 @@ function Card({ id,text,boxtext,boximg }){
   else{
     dir = "column"
   }
+
+  
   return (
     <>
       <div
@@ -80,13 +145,20 @@ function Card({ id,text,boxtext,boximg }){
         }}
       >
         {firstBox}
-        <Line id={id} boxtext={boxtext}/>
+        <Line boxtext={boxtext} isHovered={isHovered} setIsHovered={setIsHovered}/>
         {secondBox}
       </div>
     </>
   )
 }
-function Box({ id,children }){
+function Box({ children,isHovered }){
+  let style = {}
+  if(isHovered){ 
+    style = {opacity: "1"}
+  }
+  else{
+    style = {opacity: "0"}
+  }
   return (
     <>
       <div
@@ -96,9 +168,9 @@ function Box({ id,children }){
           width: "100%", 
           opacity: "0%",
           transition: "opacity 1s cubic-bezier(0.01, 0.37, 0.01, 0.99)",
-          minHeight: "10px"
+          minHeight: "10px",
+          ...style
         }}
-        className={id}
       >
         {children}
       </div>
@@ -118,7 +190,7 @@ function Boximg({boximg}){
     </>
   )
 }
-function Line({ id,boxtext }){
+function Line({ boxtext,isHovered,setIsHovered }){
   const { width, height } = checkWindowDimension();
   let textstuff;
   let wrap;
@@ -140,7 +212,7 @@ function Line({ id,boxtext }){
           position: "relative"
         }}
       >
-        <Square color={"white"} border={"black"} id={id}>
+        <Square color={"white"} border={"black"} isHovered={isHovered} setIsHovered={setIsHovered}>
           <p
           style={{
             fontSize: textstuff,
@@ -153,81 +225,55 @@ function Line({ id,boxtext }){
     </>
   )
 }
-function Square({color,border,id,children}){
-  let w;
-  let h;
+function Square({color,border,children,isHovered,setIsHovered}){
+  let size;
   const { width, height } = checkWindowDimension();
   if(width < 600){
-    w = "20px";
-    h = "70px";
+    size = {w: 20,h: 70, m: 5}
   }
   else{
-    w = "50px";
-    h = "50px";
+    size = {w: 50,h: 50, m: 10}
   }
+
+  let endwidth;
+  if(isHovered){
+    endwidth = size.w*size.m;
+  }
+  else{
+    endwidth = size.w
+  }
+
+  let squareStyle = {
+    height: size.h + "px",
+    width: endwidth + "px",
+    backgroundColor: color,
+    border: "0.5rem solid " + border,
+    zIndex: "2",
+    position: "absolute",
+    top: "50%",            
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    transition: "width 1s cubic-bezier(0.01, 0.37, 0.01, 0.99)",
+    overflow: "hidden",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+
   return(
     <div
-      style={{
-        height: h,
-        width: w,
-        backgroundColor: color,
-        border: "0.5rem solid " + border,
-        zIndex: "2",
-        position: "absolute",
-        top: "50%",            
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        transition: "width 1s cubic-bezier(0.01, 0.37, 0.01, 0.99)",
-        overflow: "hidden",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-      onMouseEnter={()=>{testfunc(id,width,height)}}
-      onMouseLeave={()=>{testfunc(id,width,height)}}
-      onTouchStart={()=>{testfunc(id,width,height)}}
-      onTouchEnd={()=>{testfunc(id,width,height)}}
-      id={id + "Square"}
+      style={
+        squareStyle
+      }
+      onMouseEnter={()=>{setIsHovered(true)}}
+      onMouseLeave={()=>{setIsHovered(false)}}
+      onTouchStart={()=>{setIsHovered(true)}}
+      onTouchEnd={()=>{setIsHovered(false)}}
     >
       {children}
     </div>
   )
 }
 
-function testfunc(id,width,height){
-  let w;
-  let h;
-  let m;
-  if(width < 600){
-    w = "20"
-    h = "70"
-    m = "5"
-  }
-  else{
-    w = "50"
-    h = "50"
-    m = "10"
-  }
-  let test = document.getElementsByClassName(id);
-  for (let t of test) {
-    if(t.style.opacity == 0){
-      t.style.opacity = 1;
-    }
-    else{
-      t.style.opacity = 0;
-    }
-    
-  }
-  let square = document.getElementById(id+"Square");
-  
-  if(parseInt(square.style.width) > w){
-    console.log("Return back to og size")
-    square.style.width = w+"px";
-    square.style.height = h+"px";
-  }
-  else{
-    square.style.width = w*m+"px";
-    square.style.height = h+"px";
-  }
-}
+
 export default App
